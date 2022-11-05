0b6406df9fc92a1be427d887f6c273efa1db971e
UpstairsForest <nikita.batalov@uzh.ch>
2022-11-05 14:37:45 +0100

    non-interactive truncate

--- client/src/components/Product/Product.js
+++ client/src/components/Product/Product.js
@@ -1,11 +1,12 @@
 import React from "react";
+import truncate from "../../helpers";
 
 const Product = (props) => {
     return (
         <div>
             <img src={props.productData.image}  alt={""}/>
             <div>{props.productData.title}</div>
-            <div>{props.productData.description}</div>
+            <div>{truncate(props.productData.description)}</div>
             <div>{props.productData.priceOrig}</div>
             <div>{props.productData.priceDisc}</div>
             <div>{props.productData.expDate}</div>
