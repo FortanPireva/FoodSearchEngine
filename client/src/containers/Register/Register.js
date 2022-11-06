89c7090066135ede4b92d148696a8617a3e99542
UpstairsForest <nikita.batalov@uzh.ch>
2022-11-06 05:47:56 +0100

    some number type check

--- client/src/containers/Register/Register.js
+++ client/src/containers/Register/Register.js
@@ -107,6 +107,7 @@ export default function Register() {
                             margin="normal"
                             required
                             fullWidth
+                            type="number"
                             id="phoneNumber"
                             label="Phone Number"
                             name="phoneNumber"
