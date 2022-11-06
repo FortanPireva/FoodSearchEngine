b44bdccf39d4f5ab48c4044aa53847af59b9c2e9
raeygetahun <getahunraey@gmail.com>
2022-11-06 08:04:02 +0100

    mission

--- client/src/pages/OurMission/OurMission.js
+++ client/src/pages/OurMission/OurMission.js
@@ -1,7 +1,61 @@
 import React from "react";
 
 const OurMission = () => {
-  return <div>OurMission</div>;
-};
+  return (
+    <><div>
+      <h1>
+        Our Mission
+        <hr />
+      </h1>
+    </div>
+    <span> General food waste statistics: </span>
+    <ul>
+      <li>Worldwide: Around 1.3 billion tons of the world's food ends up in the trash every year.</li>
+      <li>Germany: In Germany alone, around 12 million tons of food are wasted every year.</li>
+      <li>Per capita: On average, we Germans throw around 75 kilograms of our food in the trash can!</li>
+      <li>Private households: Around 6.7 million tons of still edible food are thrown away in German households. 
+        Figuratively speaking, that's about two fully packed shopping carts with a commodity value of 234 euros per person.</li>
+      <li>One in eight foods: In German households, around one in eight items of food is thrown away - 
+        even though no one actually wants to waste food.</li>
+      <li>Production to consumption: In fact, around one third of all food produced worldwide is lost on its way 
+        from the field to the plate.</li>
+    </ul>
+    <span>A report by the Johann Heinrich von Thünen Institute reveals which areas are particularly responsible 
+      for the shocking figures on food waste in Germany:</span>
+    <ul>
+      <li>Private households: About 52 percent of all food waste is generated in private households.</li>
+      <li>Agriculture: Primary production accounts for about 12 percent (1.4 million tons in total).</li>
+      <li>Processing: In the processing of food alone, about 18 percent - and thus about 2.2 million metric 
+        tons - of food is lost.</li>
+      <li>Retail: Retailers generate 4 percent (0.5 million tons) of all food waste in Germany.</li>
+      <li>Out-of-home consumption: About 14 percent (1.7 million tons) of food waste is attributable to ToGo consumption.</li>
+    </ul>
+    <span>All of the above statistics should inspire reflection. But some of the facts also raise moral questions, 
+      as food waste occurs at the expense of the planet, animals, 
+      or even other people. What do we mean by that? See for yourself - here are some facts from a study by the 
+      Federal Environment Agency and other organizations:</span>
+    <ul>
+      <li>Greenhouse gases: Every year, food waste generates more than 38 million metric tons of greenhouse gases, 
+        which contribute significantly to the Climate change Contribute.</li>
+      <li>Land consumption: About 43,000 square kilometers of agricultural land are tilled, only to have the food thrown away.</li>
+      <li>Water consumption: About 216 million cubic meters of water are consumed in vain. This contributes 
+        decisively to the Water shortage.</li>
+      <li>Animal Welfare: According to the Heinrich Böll Foundation, more than four kilograms of meat and sausage 
+        are wasted per capita in Germany every year. Converted to slaughter animals, 
+        that's 230,000 cattle, 1,800,000 ducks, 2,700,000 turkeys, 4,100,000 pigs and 45,000,000 chickens carelessly 
+        thrown into the garbage can.</li>
+    </ul>
+    <span></span>
+    <div>
+      Our mission is to solve this problem by providing this platform where markets and people add products that 
+      soon expire as a give away or a sell and where users get products they are looking for.    
+
+    </div>
+    <div style={{height:'100px'}}>
+
+    </div>
+    </>
+  );
+  };
 
 export default OurMission;
